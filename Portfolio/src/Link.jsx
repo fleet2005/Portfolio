3b06fbd11dd6090f5d44a378a32134fe08788bfe
fleet2005 <100420vishalsrinivasan.gbkm@gmail.com>
2025-03-28 13:09:44 +0530

    Modified Navbar

--- Portfolio/src/Link.jsx
+++ Portfolio/src/Link.jsx
@@ -10,10 +10,9 @@ function Link(){
         <div id="linkSection">
             <br/>
             <div id="links" style={{display: "flex", justifyContent: "center"}}>
-                <a href = "https://www.codechef.com/users/code_fleet_05" target="_blank"><img title="codechef" src={codechef} style={{height:"4vw", marginRight:"5vw", borderStyle:"solid", borderColor:"green", borderRadius:"1vw", padding:"0.2vw", outlineStyle:'inset'}}/></a> 
                 <a href = "https://www.linkedin.com/in/vishal-srinivasan-661ab5257/" target="_blank"><img title="linkedin" src={linkedin} style={{height:"4vw", marginRight:"5vw",borderStyle:"solid", borderColor:"green", borderRadius:"1vw", padding:"0.2vw",outlineStyle:'inset'}}/></a> 
-                <a href = "https://github.com/fleet2005" target="_blank"><img title= "github"  src={github} style={{height:"4vw", marginRight:"5vw",borderStyle:"solid", borderColor:"green", borderRadius:"1vw", padding:"0.2vw",outlineStyle:'inset'}}/></a> 
-                <a href = "https://leetcode.com/Code_Fleet_2005/" target="_blank"><img title="leetcode" src={leetcode} style={{height:"4vw",borderStyle:"solid", borderColor:"green", borderRadius:"1vw", padding:"0.2vw", outlineStyle:'inset'}}/></a> 
+                <a href = "https://leetcode.com/Code_Fleet_2005/" target="_blank"><img title="leetcode" src={leetcode} style={{height:"4vw",borderStyle:"solid", marginRight:"5vw", borderColor:"green", borderRadius:"1vw", padding:"0.2vw", outlineStyle:'inset'}}/></a> 
+                <a href = "https://github.com/fleet2005" target="_blank"><img title= "github"  src={github} style={{height:"4vw",borderStyle:"solid", borderColor:"green", borderRadius:"1vw", padding:"0.2vw",outlineStyle:'inset'}}/></a> 
             </div>
 
         </div>
--- Portfolio/src/NavigationBar.jsx
+++ Portfolio/src/NavigationBar.jsx
@@ -10,16 +10,18 @@ function NavigationBar() {
     }
 
     return (
-
+        
         <ul id="NavList" style={{ backgroundColor: "black", opacity: "0.8", position: "sticky", top:"0"}}>
-            <li> <a href={"#"} style={{ textDecoration: "none", color: "white", transition: "" }}><img src={homeIcon} style={{ marginRight: "auto", marginTop: "-0.5vw", marginLeft: "auto", width: "4vw", height: "4vw" }} /></a></li>
+            <a href={"#"} style={{ textDecoration: "none", color: "white", transition: "" , marginLeft: "2.4vw"}}><img src={homeIcon} style={{ marginRight: "auto", marginTop: "-0.5vw", marginLeft: "-5vw", width: "4vw", height: "4vw" }} /></a>
             <li> <a href={"#aboutSection"} style={{ textDecoration: "none", color: "white", transition: "" }}>About</a></li>
+            <li> <a href={"#workSection"} style={{ textDecoration: "none", color: "white", transition: "" }}>Work Experience</a></li>
             <li> <a href={"#projects"} style={{ textDecoration: "none", color: "white", transition: "" }}>Projects</a></li>
             <li> <a href={"#skillSection"} style={{ textDecoration: "none", color: "white", transition: "" }}>Skills</a></li>
+            <li> <a href={"#awards"} style={{ textDecoration: "none", color: "white", transition: "" }}>Awards</a></li>
             <li> <a href={"#contactSection"} style={{ textDecoration: "none", color: "white", transition: "" }}> Contact </a> </li>
-            <li> <a href={"#linkSection"} style={{ textDecoration: "none", color: "white", transition: "" }}> Links </a> </li>
-            <li style={{ marginRight: "4vw", marginTop: "-0.5vw" }} onClick={refresh}><img src={refreshIcon} style={{ width: "3.5vw", height: "3.5vw" }} /></li>
+            <li style={{ marginRight: "1.3vw", marginTop: "-0.5vw" }} onClick={refresh}><img src={refreshIcon} style={{ width: "3.5vw", height: "3.5vw" }} /></li>
         </ul>
+
     );
 }
 
