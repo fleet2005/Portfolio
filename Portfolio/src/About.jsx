c8fe6c60ccbfce120a7dab4eb7dbee4d3bc16da4
fleet2005 <100420vishalsrinivasan.gbkm@gmail.com>
2025-09-11 01:21:48 +0530

    Modified About Page

--- Portfolio/src/About.jsx
+++ Portfolio/src/About.jsx
@@ -13,8 +13,8 @@ function About()
 
         <div className="container scroll-fade-in" ref={aboutRef} id = "aboutSection">
             <h1 className="scroll-fade-in" ref={titleRef}>😺 ABOUT ME 😎 </h1>
-            <img className="scroll-slide-right" ref={imageRef} src = {Vishal_pic} alt="Profile_Pic" title="VISHAL SRINIVASAN" style={{width:"24vw", float: "right", marginLeft:"4vw", height:"31vw", marginRight:"-20vw"}}></img>
-            <p className="scroll-slide-left" ref={textRef}> Hello! 👋 My name is Vishal Srinivasan, and I'm thrilled to welcome you to my page. As a dedicated full stack web developer, I have a strong affinity for both Frontend and Backend Development. I'm deeply passionate about developing web applications, software and algorithms. Constant learning and growth are at the core of my journey, as I strive to enhance my skills and knowledge. While I predominantly focus on the MERN stack, I also have lots of experience in MySQL databases. Do connect with me on linkedin!</p>
+            <img className="scroll-slide-right" ref={imageRef} src = {Vishal_pic} alt="Profile_Pic" title="VISHAL SRINIVASAN" style={{width:"23vw", float: "right", marginLeft:"4vw", height:"27vw", marginRight:"-20vw"}}></img>
+            <p className="scroll-slide-left" ref={textRef}> Hello! 👋 My name is Vishal Srinivasan, and I'm thrilled to welcome you to my page. As a dedicated full stack web developer, I have a strong affinity for both Frontend and Backend Development. I'm deeply passionate about developing web applications, software and algorithms. I am a person who gets work done at all costs and I am not limited by my tech stack. I can learn new tech stacks rapidly. Do connect with me on linkedin! </p>
         </div>
 
     );
